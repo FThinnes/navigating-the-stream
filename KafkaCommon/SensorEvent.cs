017c4250020ebee6f28f53353ef290cbe6fdbffb
Tobias Braun <braun.tob@herrenknecht.com>
2025-01-24 16:00:30 +0100

    Initial commit

---
+++ KafkaCommon/SensorEvent.cs
@@ -0,0 +1,8 @@
+﻿namespace KafkaCommon;
+
+public class SensorEvent
+{
+    public long Timestamp { get; set; }
+    public long SensorId { get; set; }
+    public double NewSensorValue { get; set; }
+}
